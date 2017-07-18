32291e8e3043dbba90050d4a545dda6a6ce651d2
8c6794b6 <8c6794b6@gmail.com>
2017-07-18 19:41:23 +0900

    Add `eval-when-compile' special form
    Add `eval-when-compile' special form to initial SK environment. This
    special form modifies interactive context of compiler session with
    declarations written in its body.
    
    Add simple test containing `eval-when-compile'.

--- src/SK/Core/Macro.hs
+++ src/SK/Core/Macro.hs
@@ -12,13 +12,33 @@ module SK.Core.Macro
 import Control.Monad (foldM, when)
 import Unsafe.Coerce (unsafeCoerce)
 
+-- ghc
+import ByteCodeGen (byteCodeGen)
+import ConLike (ConLike(..))
+import CorePrep (corePrepPgm)
+import CoreSyn (bindersOfBinds)
+import Desugar (deSugar)
+import ErrUtils (Messages)
+import HscMain (hscSimplify)
+import HscTypes ( CgGuts(..), ModDetails(..), ModGuts(..)
+                , extendInteractiveContext )
+import Id (idName, isDFunId, isImplicitId)
+import InteractiveEval (compileParsedExprRemote)
+import Linker (linkDecls)
+import Name (isExternalName)
+import SrcLoc (srcLocSpan)
+import TcRnDriver (tcRnDeclsi)
+import TcRnTypes (TcGblEnv(..))
+import TidyPgm (tidyProgram)
+import TyCon (isDataTyCon, isImplicitTyCon)
+import Util (filterOut)
+
 -- Internal
+import SK.Core.Builder (HExpr)
 import SK.Core.Form
 import SK.Core.GHC
-import SK.Core.Syntax (evalBuilder, parseExpr)
+import SK.Core.Syntax (evalBuilder, parseExpr, parseModule)
 import SK.Core.SKC
-
-import InteractiveEval (compileParsedExprRemote)
 
 -- ---------------------------------------------------------------------
 --
@@ -115,7 +135,7 @@ quasiquote orig@(LForm (L l form)) =
 -- with `import', for macro expansion time, done with `require'.
 
 -- This function uses `unsafeCoerce'.
-compileMT :: LHsExpr RdrName -> Skc Macro
+compileMT :: HExpr -> Skc Macro
 compileMT expr = do
   fhv <- compileParsedExprRemote expr
   hv <- liftIO (withForeignRef fhv localRef)
@@ -207,6 +227,90 @@ addImportedMacro ty_thing =
 
 -- ---------------------------------------------------------------------
 --
+-- eval-when-compile
+--
+-- ---------------------------------------------------------------------
+
+eval_when_compile :: [Code] -> Skc Code
+eval_when_compile body = do
+  -- Mostly doing similar works done in `HscMain.hscDeclsWithLocation',
+  -- but this function is wrapped with 'Skc' instead of 'Hsc'. Also,
+  -- 'hscDeclsWithlocation' is not exported, and takes 'String' of
+  -- declarations codes written in Haskell.
+  expanded <- macroexpands body
+  case evalBuilder parseModule expanded of
+    Right (HsModule { hsmodDecls = decls }) -> do
+      hsc_env <- getSession
+      tc_gblenv <- ioMsgMaybe (tcRnDeclsi hsc_env decls)
+      let defaults = tcg_default tc_gblenv
+          interactive_loc =
+             ModLocation { ml_hs_file = Nothing
+                         , ml_hi_file = error "ewc:ml_hi_file"
+                         , ml_obj_file = error "ewc:ml_obj_file"}
+      ds_result <- skcDesugar' interactive_loc tc_gblenv
+      simpl_mg <- liftIO (hscSimplify hsc_env ds_result)
+      (tidy_cg, mod_details) <- liftIO (tidyProgram hsc_env simpl_mg)
+      let CgGuts { cg_module = this_mod
+                 , cg_binds = core_binds
+                 , cg_tycons = tycons
+                 , cg_modBreaks = mod_breaks } = tidy_cg
+          ModDetails { md_insts = cls_insts
+                     , md_fam_insts = fam_insts } = mod_details
+          data_tycons = filter isDataTyCon tycons
+      prepd_binds <-
+        liftIO (corePrepPgm hsc_env this_mod interactive_loc core_binds
+                            data_tycons)
+      cbc <- liftIO (byteCodeGen hsc_env this_mod prepd_binds
+                                 data_tycons mod_breaks)
+      let src_span = srcLocSpan evalWhenCompileSrcLoc
+      liftIO (linkDecls hsc_env src_span cbc)
+
+      -- Not done in ghc-8.0.2.
+      -- liftIO (hscAddSptEntries hsc_env (cg_spt_entries tidy_cg))
+
+      let tcs = filterOut isImplicitTyCon (mg_tcs simpl_mg)
+          patsyns = mg_patsyns simpl_mg
+          ext_ids = [ id' | id' <- bindersOfBinds core_binds
+                           , isExternalName (idName id')
+                           , not (isDFunId id' || isImplicitId id')]
+          new_tythings = map AnId ext_ids ++ map ATyCon tcs ++
+                         map (AConLike . PatSynCon) patsyns
+          ictxt = hsc_IC hsc_env
+          fix_env = tcg_fix_env tc_gblenv
+          new_ictxt = extendInteractiveContext ictxt new_tythings
+                                               cls_insts fam_insts
+                                               defaults fix_env
+      setSession (hsc_env { hsc_IC = new_ictxt })
+      --  return (new_tythings, new_ictxt)
+      return ()
+    Left err ->
+      let L l forms = mkLocatedForm body
+      in  skSrcError (LForm (L l (List forms))) err
+  return emptyForm
+
+-- GHC head exports this from HscMain, but 8.0.2 doesn't.
+ioMsgMaybe :: IO (Messages, Maybe a) -> Skc a
+ioMsgMaybe ioA = do
+  ((_warns, _errs), mb_r) <- liftIO ioA
+  debugIO (putStrLn "ioMsgMaybe")
+  case mb_r of
+    Nothing -> error "ioMsgMaybe"
+    Just r  -> return r
+
+-- Like 'HscMain.hscDesugar'', but for 'SKC'.
+skcDesugar' :: ModLocation -> TcGblEnv -> Skc ModGuts
+skcDesugar' mod_location tc_result = do
+  hsc_env <- getSession
+  r <- ioMsgMaybe (deSugar hsc_env mod_location tc_result)
+  -- handleWarnings
+  return r
+
+evalWhenCompileSrcLoc :: SrcLoc
+evalWhenCompileSrcLoc = UnhelpfulLoc (fsLit "<eval-when-compile>")
+
+
+-- ---------------------------------------------------------------------
+--
 -- Special forms
 --
 -- ---------------------------------------------------------------------
@@ -289,11 +393,6 @@ m_require form =
         Nothing -> skSrcError form ("require: cannot find modinfo for "
                                  ++ mname)
     _ -> skSrcError form "require: malformed syntax."
-
-eval_when_compile :: [Code] -> Skc Code
-eval_when_compile body = do
-  liftIO (putStrLn (";;; eval-when-compile: body = " ++ show body))
-  return emptyForm
 
 m_evalWhenCompile :: Macro
 m_evalWhenCompile form =
