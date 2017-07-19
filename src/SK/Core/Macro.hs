e59dd48bb9bd708823be2b65ff6d625589b82e55
8c6794b6 <8c6794b6@gmail.com>
2017-07-19 16:06:17 +0900

    Throw exception on errors while evaluating declarations
    Throw source code error exception from `ioMsgMaybe' function when
    resulting value was `Nothing'. Add comments for warning messages.

--- src/SK/Core/Macro.hs
+++ src/SK/Core/Macro.hs
@@ -279,17 +279,17 @@ compileDecls decls = do
       new_ictxt = extendInteractiveContext ictxt new_tythings
                                            cls_insts fam_insts
                                            defaults fix_env
-      dflags = hsc_dflags hsc_env
   setSession (hsc_env {hsc_IC = new_ictxt})
   return (new_tythings, new_ictxt)
 
 -- GHC head exports this from HscMain, but 8.0.2 doesn't.
 ioMsgMaybe :: IO (Messages, Maybe a) -> Skc a
 ioMsgMaybe ioA = do
-  ((_warns, _errs), mb_r) <- liftIO ioA
+  -- XXX: Show warning messages with DynFlags settings.
+  ((_warns, errs), mb_r) <- liftIO ioA
   debugIO (putStrLn "ioMsgMaybe")
   case mb_r of
-    Nothing -> error "ioMsgMaybe"
+    Nothing -> liftIO (throwIO (mkSrcErr errs))
     Just r  -> return r
 
 -- Like 'HscMain.hscDesugar'', but for 'SKC'.
@@ -297,7 +297,12 @@ skcDesugar' :: ModLocation -> TcGblEnv -> Skc ModGuts
 skcDesugar' mod_location tc_result = do
   hsc_env <- getSession
   r <- ioMsgMaybe (deSugar hsc_env mod_location tc_result)
+
+  -- In `Hsc', `handleWarning' is called at this point. But currently
+  -- Skc does not keep tracks of warning messages, so does nothing ...
+  --
   -- handleWarnings
+
   return r
 
 compileDeclsSrcLoc :: SrcLoc
