ac8364944da6b5ba06a63f15dde0a1f276bc0d19
8c6794b6 <8c6794b6@gmail.com>
2017-07-31 16:31:57 +0900

    Add test for `make' function
    Add test for make, compile SK source code via `make' function. Add
    simple SK source code to compile, add this file to cabal package's extra
    source files.

--- test/BuildTest.hs
+++ test/BuildTest.hs
@@ -1,10 +1,33 @@
 module BuildTest where
 
+-- base
+import Control.Monad (when)
+import System.Directory (doesFileExist, removeFile)
 import System.Exit (ExitCode(..))
-import System.FilePath ((</>))
+import System.FilePath ((</>), replaceExtension)
 import System.Process (rawSystem)
 
+-- hspec
 import Test.Hspec
+
+-- sk-core
+import SK.Core.Make
+import SK.Core.Run
+
+removeIfExist :: FilePath -> IO ()
+removeIfExist path = do
+  exist <- doesFileExist path
+  when exist (removeFile path)
+
+buildFile :: [FilePath] -> Spec
+buildFile paths =
+  describe ("compile files: " ++ show paths) $
+    it "should compile successfully" $ do
+      let targets = map (\path -> (path, Nothing)) paths
+          rm path ext = removeIfExist (replaceExtension path ext)
+      mapM_ (\path -> rm path ".o" >> rm path ".hi") paths
+      ret <- runSkc (make targets True Nothing) initialSkEnv
+      ret `shouldBe` Right ()
 
 stack :: String -> [String] -> IO ExitCode
 stack projectName args = do
@@ -12,8 +35,8 @@ stack projectName args = do
              "stack.yaml"
   rawSystem "stack" (("--stack-yaml=" ++ yaml):"--silent":args)
 
-buildOne :: String -> Spec
-buildOne name =
+buildPackage :: String -> Spec
+buildPackage name =
   describe ("package " ++ name) $
     it "should compile and pass the tests" $ do
       let s = stack name
@@ -25,4 +48,6 @@ buildOne name =
       exitCode `shouldBe` ExitSuccess
 
 buildTests :: Spec
-buildTests = buildOne "p01"
+buildTests =
+  do buildFile ["test/data/build/m1.sk"]
+     buildPackage "p01"
