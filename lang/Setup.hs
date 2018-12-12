33f114e16fb3a7664f7461bd73933b0641b35b6c
8c6794b6 <8c6794b6@gmail.com>
2018-12-12 16:50:44 +0900

    Detect build tool by executable path
    Modify "Setup.hs" used in sk-lang package, detect the build tool in
    use (stack or cabal) by looking up ".stack-work" string in executable
    path of compiled setup script.

--- lang/Setup.hs
+++ lang/Setup.hs
@@ -1,24 +1,21 @@
 module Main where
 
-import System.Environment (lookupEnv)
+import Data.List (isSubsequenceOf)
+import System.Environment (getExecutablePath)
 import Distribution.Simple.SK (defaultMainWithHooks, skcHooksWith)
 
 main :: IO ()
 main = decideHooks >>= defaultMainWithHooks
   where
-    -- Stack sets "GHC_PACKAGE_PATH" environment variable during
-    -- build. Cabal nix-style commands uses "GHC_ENVIRONMENT"
-    -- environment variable, and will complain if "GHC_PACKAGE_PATH"
-    -- were set.
-    --
-    -- So, first check whether the GHC_PACKAGE_PATH exist in current
-    -- environment variable, if exist, assume as running via
-    -- stack. Otherwise assume as cabal nix style build.
+    -- Deciding whether this package is compiled with "stack" or
+    -- "cabal". Assuming that stack uses a build directory path
+    -- containing ".stack-work".
     decideHooks = do
-      mb_ghc_pkg_path <- lookupEnv "GHC_PACKAGE_PATH"
-      case mb_ghc_pkg_path of
-        Just _  -> return stackHook
-        Nothing -> return cabalHook
-    stackHook = skcHooksWith "skkc2" [] False
+      exec_path <- getExecutablePath
+      if ".stack-work" `isSubsequenceOf` exec_path
+         then return stackHook
+         else return cabalHook
+    stackHook =
+      skcHooksWith "skkc2" [] False
     cabalHook =
       skcHooksWith "cabal" ["new-run", "-v0", "--", "skkc2"] False
