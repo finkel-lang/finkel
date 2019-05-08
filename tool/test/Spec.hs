9fb7f9061f02ccb151953c17c11e189c831ea591
8c6794b6 <8c6794b6@gmail.com>
2019-05-08 21:32:46 +0900

    Add some tests for main command line
    Add "MainTest" module in "sk-tool" package, to run some command line
    argument handling works.

--- tool/test/Spec.hs
+++ tool/test/Spec.hs
@@ -1,9 +1,11 @@
 module Main where
 
+import MainTest
 import ReplTest
 import SetupTest
 import Test.Hspec
 
 main :: IO ()
-main = hspec (do replTests
+main = hspec (do mainTest
+                 replTests
                  setupTests)
