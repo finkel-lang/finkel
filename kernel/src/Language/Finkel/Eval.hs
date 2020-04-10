afbabfb1523699d0e04b3712296788c77447aa2b
8c6794b6 <8c6794b6@gmail.com>
2020-04-10 18:40:06 +0900

    Use hscParsedDecls in evalDecls
    Modify "evalDecls" to reuse "hscParsedDecls" for ghc >= 8.8.0. Move around some
    auxiliary top-level functions.

--- kernel/src/Language/Finkel/Eval.hs
+++ kernel/src/Language/Finkel/Eval.hs
@@ -10,39 +10,20 @@ module Language.Finkel.Eval
 
 -- base
 import Control.Monad.IO.Class  (liftIO)
-#if MIN_VERSION_ghc(8,4,0)
+
+#if MIN_VERSION_ghc(8,4,0) && !MIN_VERSION_ghc(8,8,0)
 import Data.IORef              (readIORef)
 #endif
 
 -- ghc
-import ByteCodeGen             (byteCodeGen)
-import ConLike                 (ConLike (..))
-import CorePrep                (corePrepPgm)
-import CoreSyn                 (CoreProgram, bindersOfBinds)
-import Desugar                 (deSugar)
 import ErrUtils                (Messages)
 import Exception               (throwIO)
-import FastString              (fsLit)
 import GhcMonad                (GhcMonad (..))
-import HscMain                 (hscAddSptEntries, hscSimplify)
-import HscTypes                (CgGuts (..), HscEnv (..),
-                                InteractiveContext (..), ModDetails (..),
-                                ModGuts (..), TyThing (..),
-                                extendInteractiveContext, mkSrcErr)
-import Id                      (idName, isDFunId, isImplicitId)
+import HscTypes                (HscEnv (..), InteractiveContext (..),
+                                TyThing (..), mkSrcErr)
 import InteractiveEval         (compileParsedExprRemote)
-import Linker                  (linkDecls)
-import Module                  (ModLocation (..), Module, moduleName,
-                                moduleNameString)
-import Name                    (isExternalName)
-import SrcLoc                  (SrcLoc (..), srcLocSpan)
-import TcRnDriver              (TcRnExprMode (..), tcRnDeclsi, tcRnExpr,
-                                tcRnType)
-import TcRnTypes               (TcGblEnv (..))
-import TidyPgm                 (tidyProgram)
-import TyCon                   (TyCon, isDataTyCon, isImplicitTyCon)
+import TcRnDriver              (TcRnExprMode (..), tcRnExpr, tcRnType)
 import TyCoRep                 (Kind, Type (..))
-import Util                    (filterOut)
 import VarEnv                  (emptyTidyEnv)
 
 #if MIN_VERSION_ghc(8,10,0)
@@ -50,6 +31,31 @@ import TcHsSyn                 (ZonkFlexi (..))
 import TyCoTidy                (tidyType)
 #else
 import TyCoRep                 (tidyType)
+#endif
+
+#if MIN_VERSION_ghc(8,8,0)
+import HscMain                 (hscParsedDecls)
+#else
+import ByteCodeGen             (byteCodeGen)
+import ConLike                 (ConLike (..))
+import CorePrep                (corePrepPgm)
+import CoreSyn                 (CoreProgram, bindersOfBinds)
+import Desugar                 (deSugar)
+import FastString              (fsLit)
+import HscMain                 (hscAddSptEntries, hscSimplify)
+import HscTypes                (CgGuts (..), ModDetails (..), ModGuts (..),
+                                extendInteractiveContext)
+import Id                      (idName, isDFunId, isImplicitId)
+import Linker                  (linkDecls)
+import Module                  (ModLocation (..), Module, moduleName,
+                                moduleNameString)
+import Name                    (isExternalName)
+import SrcLoc                  (SrcLoc (..), srcLocSpan)
+import TcRnDriver              (tcRnDeclsi)
+import TcRnTypes               (TcGblEnv (..))
+import TidyPgm                 (tidyProgram)
+import TyCon                   (TyCon, isDataTyCon, isImplicitTyCon)
+import Util                    (filterOut)
 #endif
 
 -- ghci
@@ -93,8 +99,8 @@ evalTypeKind :: HType -> Fnk (Type, Kind)
 evalTypeKind ty = do
   -- See `InteractiveEval.typeKind' and `HscMain.hscKcType'.
   --
-  -- XXX: The second argument passed to `tcRnType' is hard coded as
-  -- `True' in below code.
+  -- XXX: The second argument of `tcRnType' is hard coded as `True' in below
+  -- code.
   --
   hsc_env <- getSession
   ioMsgMaybe $ tcRnType' hsc_env True ty
@@ -102,6 +108,13 @@ evalTypeKind ty = do
 -- | Evaluate given declarations. The returned value is resulting
 -- 'TyThing's of declarations and updated interactive context.
 evalDecls :: [HDecl] -> Fnk ([TyThing], InteractiveContext)
+#if MIN_VERSION_ghc(8,8,0)
+evalDecls decls = do
+  hsc_env <- getSession
+  ret@(_tythings, ic) <- liftIO (hscParsedDecls hsc_env decls)
+  setSession (hsc_env {hsc_IC=ic})
+  return ret
+#else
 evalDecls decls = do
   -- Mostly doing similar works done in `HscMain.hscDeclsWithLocation',
   -- but this function is wrapped with 'Fnk' instead of 'Hsc'. And takes
@@ -152,15 +165,11 @@ evalDecls decls = do
       new_ictxt = extendInteractiveContext ictxt new_tythings
                                            cls_insts fam_insts
                                            defaults fix_env
+
+  -- Updating session, which is not done in hscDecls function in ghc source.
   setSession (hsc_env {hsc_IC = new_ictxt})
+
   return (new_tythings, new_ictxt)
-
-
--- ---------------------------------------------------------------------
---
--- Auxiliary
---
--- ---------------------------------------------------------------------
 
 -- | Like 'HscMain.hscDesugar'', but for 'Fnk'.
 fnkcDesugar' :: ModLocation -> TcGblEnv -> Fnk ModGuts
@@ -174,15 +183,6 @@ fnkcDesugar' mod_location tc_result = do
   -- handleWarnings
 
   return r
-
--- | Like 'HscMain.ioMsgMaybe', but for 'Fnk'.
-ioMsgMaybe :: IO (Messages, Maybe a) -> Fnk a
-ioMsgMaybe ioA = do
-  -- XXX: Show warning messages with DynFlags settings.
-  ((_warns, errs), mb_r) <- liftIO ioA
-  case mb_r of
-    Nothing -> liftIO (throwIO (mkSrcErr errs))
-    Just r  -> return r
 
 -- | GHC version compatibility helper for combining 'hscSimplify'
 -- and 'tcg_th_coreplugins'.
@@ -206,6 +206,23 @@ corePrepPgm' hsc_env this_mod mod_loc binds data_tycons =
 corePrepPgm' hsc_env this_mod mod_loc binds data_tycons =
   corePrepPgm hsc_env this_mod mod_loc binds data_tycons
 #endif
+
+#endif
+
+-- ---------------------------------------------------------------------
+--
+-- Auxiliary
+--
+-- ---------------------------------------------------------------------
+
+-- | Like 'HscMain.ioMsgMaybe', but for 'Fnk'.
+ioMsgMaybe :: IO (Messages, Maybe a) -> Fnk a
+ioMsgMaybe ioA = do
+  -- XXX: Show warning messages with DynFlags settings.
+  ((_warns, errs), mb_r) <- liftIO ioA
+  case mb_r of
+    Nothing -> liftIO (throwIO (mkSrcErr errs))
+    Just r  -> return r
 
 -- | GHC version compatibility helper for 'tcRnType'.
 tcRnType' :: HscEnv -> Bool -> HType -> IO (Messages, Maybe (Type, Kind))
