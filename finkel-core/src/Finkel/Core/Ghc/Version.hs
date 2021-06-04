3f1dcda7dd294c9a60974d2aa87fb1b513dbc287
8c6794b6 <8c6794b6@gmail.com>
2021-06-04 17:36:16 +0900

    Support :min-version in cond-expand macro
    Modify "cond-expand" macro to support ":min-version" keyword, to test the
    package version used during compilation. Rewrite the code using "cond-expand" in
    "finkel-tool" package to use relevant package versions.
    
    Add and export "caris", "trav1", "omni", and "omniM" functions from finkel-core
    package, add some tests.
    
    Some code clean ups.

--- finkel-core/src/Finkel/Core/Ghc/Version.hs
+++ finkel-core/src/Finkel/Core/Ghc/Version.hs
@@ -1,11 +1,59 @@
 {-# LANGUAGE CPP #-}
--- | Wrapper module to export 'cProjectVersion' from @ghc@ package.
+-- | Wrapper module to export version related functions.
 module Finkel.Core.Ghc.Version
   ( cProjectVersionInt
+  , getVersion
   ) where
 
+-- base
+import Data.Version        (Version)
+
+-- finkel-kernel
+import Language.Finkel     (Code, Fnk, finkelSrcError, fromCode)
+
 #if MIN_VERSION_ghc(9,0,0)
+-- ghc
+import GHC.Data.FastString (fsLit)
+import GHC.Driver.Session  (DynFlags (..))
 import GHC.Settings.Config (cProjectVersionInt)
+import GHC.Unit.State      (PackageName (..), lookupPackageName, lookupUnitId,
+                            unitPackageVersion)
+import GHC.Unit.Types      (indefUnit)
 #else
+-- ghc
 import Config              (cProjectVersionInt)
+import DynFlags            (DynFlags)
+import FastString          (fsLit)
+import Module              (componentIdToInstalledUnitId)
+import Packages            (PackageName (..), lookupInstalledPackage,
+                            lookupPackageName)
+
+-- ghc-boot
+import GHC.PackageDb       (packageVersion)
+#endif
+
+getVersion :: DynFlags -> Code -> Fnk Version
+getVersion dflags form =
+  let err = finkelSrcError form
+  in  case fromCode form of
+    Nothing -> err ("want package name `String' value but got: " ++ show form)
+    Just name -> case lookupPackageVersion dflags name of
+      Nothing -> err ("cannot find package: " ++ name)
+      Just v  -> pure v
+
+lookupPackageVersion :: DynFlags -> String -> Maybe Version
+#if MIN_VERSION_ghc(9,0,0)
+lookupPackageVersion dflags name =
+  do let pname = PackageName (fsLit name)
+         ust = unitState dflags
+     indef_uid <- lookupPackageName ust pname
+     uid <- lookupUnitId ust (indefUnit indef_uid)
+     pure $ unitPackageVersion uid
+#else
+lookupPackageVersion dflags name =
+  do let pname = PackageName (fsLit name)
+     component_id <- lookupPackageName dflags pname
+     let iuid = componentIdToInstalledUnitId component_id
+     conf <- lookupInstalledPackage dflags iuid
+     pure $ packageVersion conf
 #endif
