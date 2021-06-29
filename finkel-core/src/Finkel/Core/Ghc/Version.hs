9fb85547e661089f5c1b88ee57c01cb4d563e8ac
8c6794b6 <8c6794b6@gmail.com>
2021-06-29 21:03:43 +0900

    Modify finkel-core to support ghc-9.2.0.20210422
    Tests in "finkel-core" package is disabled at the moment.

--- finkel-core/src/Finkel/Core/Ghc/Version.hs
+++ finkel-core/src/Finkel/Core/Ghc/Version.hs
@@ -2,7 +2,8 @@
 -- | Wrapper module to export version related functions.
 module Finkel.Core.Ghc.Version
   ( cProjectVersionInt
-  , getVersion
+  , __glasgow_haskell__
+  , getPackageVersion
   ) where
 
 -- base
@@ -11,18 +12,24 @@ import Data.Version        (Version)
 -- finkel-kernel
 import Language.Finkel     (Code, Fnk, finkelSrcError, fromCode)
 
-#if MIN_VERSION_ghc(9,0,0)
 -- ghc
+#if MIN_VERSION_ghc(9,2,0)
+import GHC.Driver.Env      (HscEnv (..), hsc_units)
+#elif MIN_VERSION_ghc(9,0,0)
+import GHC.Driver.Session  (unitState)
+import GHC.Driver.Types    (HscEnv (..))
+#else
+import HscTypes            (HscEnv (..))
+#endif
+
+#if MIN_VERSION_ghc(9,0,0)
 import GHC.Data.FastString (fsLit)
-import GHC.Driver.Session  (DynFlags (..))
 import GHC.Settings.Config (cProjectVersionInt)
 import GHC.Unit.State      (PackageName (..), lookupPackageName, lookupUnitId,
                             unitPackageVersion)
 import GHC.Unit.Types      (indefUnit)
 #else
--- ghc
 import Config              (cProjectVersionInt)
-import DynFlags            (DynFlags)
 import FastString          (fsLit)
 import Module              (componentIdToInstalledUnitId)
 import Packages            (PackageName (..), lookupInstalledPackage,
@@ -32,28 +39,39 @@ import Packages            (PackageName (..), lookupInstalledPackage,
 import GHC.PackageDb       (packageVersion)
 #endif
 
-getVersion :: DynFlags -> Code -> Fnk Version
-getVersion dflags form =
+-- | Function version of @__GLASGOW_HASKELL__@ C preprocessor macro.
+__glasgow_haskell__ :: Int
+__glasgow_haskell__ = __GLASGOW_HASKELL__
+
+getPackageVersion :: HscEnv -> Code -> Fnk Version
+getPackageVersion hsc_env form =
   let err = finkelSrcError form
   in  case fromCode form of
     Nothing -> err ("want package name `String' value but got: " ++ show form)
-    Just name -> case lookupPackageVersion dflags name of
+    Just name -> case lookupPackageVersion hsc_env name of
       Nothing -> err ("cannot find package: " ++ name)
       Just v  -> pure v
 
-lookupPackageVersion :: DynFlags -> String -> Maybe Version
-#if MIN_VERSION_ghc(9,0,0)
-lookupPackageVersion dflags name =
+lookupPackageVersion :: HscEnv -> String -> Maybe Version
+#if MIN_VERSION_ghc(9,2,0)
+lookupPackageVersion hsc_env name =
   do let pname = PackageName (fsLit name)
-         ust = unitState dflags
+         us = hsc_units hsc_env
+     indef_uid <- lookupPackageName us pname
+     uid <- lookupUnitId us (indefUnit indef_uid)
+     pure $ unitPackageVersion uid
+#elif MIN_VERSION_ghc(9,0,0)
+lookupPackageVersion hsc_env name =
+  do let pname = PackageName (fsLit name)
+         ust = unitState (hsc_dflags hsc_env)
      indef_uid <- lookupPackageName ust pname
      uid <- lookupUnitId ust (indefUnit indef_uid)
      pure $ unitPackageVersion uid
 #else
-lookupPackageVersion dflags name =
+lookupPackageVersion hsc_env name =
   do let pname = PackageName (fsLit name)
-     component_id <- lookupPackageName dflags pname
+     component_id <- lookupPackageName (hsc_dflags hsc_env) pname
      let iuid = componentIdToInstalledUnitId component_id
-     conf <- lookupInstalledPackage dflags iuid
+     conf <- lookupInstalledPackage (hsc_dflags hsc_env) iuid
      pure $ packageVersion conf
 #endif
