35bbb99410e62610c358970c26cae2689575df18
8c6794b6 <8c6794b6@gmail.com>
2023-08-30 17:19:16 +0900

    Remove commented out codes from Language.Finkel.Data.FastString

--- finkel-kernel/src/Language/Finkel/Data/FastString.hs
+++ finkel-kernel/src/Language/Finkel/Data/FastString.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE CPP #-}
 
-#include "ghc_modules.h"
-
--- Version compatibility module for GHC.Data.FastString
+-- | Version compatibility module for GHC.Data.FastString
 module Language.Finkel.Data.FastString
   ( module FS
 #if !MIN_VERSION_ghc(9,2,0)
@@ -10,25 +8,10 @@ module Language.Finkel.Data.FastString
 #endif
   ) where
 
+#include "ghc_modules.h"
+
+-- ghc
 import GHC_Data_FastString as FS
-
--- #if MIN_VERSION_ghc(9,0,0) && !MIN_VERSION_ghc(9,2,0)
--- import qualified Data.ByteString.Char8 as C8
--- #endif
-
--- #if !MIN_VERSION_ghc(9,2,0)
--- unconsFS :: FastString -> Maybe (Char, FastString)
--- #  if MIN_VERSION_ghc(9,0,0)
--- unconsFS fs =
---   if nullFS fs then
---     Nothing
---   else
---     Just (headFS fs, mkFastStringByteString (C8.tail (bytesFS fs)))
--- #  else
--- unconsFS fs = if nullFS fs then Nothing else Just (headFS fs, tailFS fs)
--- #  endif
--- {-# INLINABLE unconsFS #-}
--- #endif
 
 #if !MIN_VERSION_ghc(9,2,0)
 unconsFS :: FastString -> Maybe (Char, FastString)
