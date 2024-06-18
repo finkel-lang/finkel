59efef8b97f3a329b2fa0ad9fc3fb2b9107fe747
8c6794b6 <8c6794b6@gmail.com>
2024-06-18 15:54:15 +0900

    Use fnkPluginMainForHaddock in finkel-core package

--- finkel-core/Setup.hs
+++ finkel-core/Setup.hs
@@ -1,2 +1,9 @@
+{-# LANGUAGE CPP #-}
+
+#if 906 <= __GLASGOW_HASKELL__
+import Distribution.Simple.Finkel
+main = fnkPluginMainForHaddock
+#else
 import Distribution.Simple (defaultMain)
 main = defaultMain
+#endif
